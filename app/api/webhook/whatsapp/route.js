ea4ce675a06c3372e39b16925675c3faa9efc93f
Your Name <you@example.com>
2026-09-25 00:50:04 +0530

    error

--- app/api/webhook/whatsapp/route.js
+++ app/api/webhook/whatsapp/route.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import { connectDB } from "../../../../db/connetion";
 import MessageStatusModel from "../../../../model/message/messageStatus.model";
 
-import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
